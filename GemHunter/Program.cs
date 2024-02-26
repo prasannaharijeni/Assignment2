a435fb94ced7824258527f2e9e8af9f65a470623
prasanna <hariprasannajeni@gmail.com>
2024-02-25 22:18:36 -0500

    Created Player class to define the name, position, and Gem count.

--- GemHunter/Program.cs
+++ GemHunter/Program.cs
@@ -13,6 +13,42 @@ class Position
     }
 }
 
+class Player
+{
+    public string Name { get; set; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+    //Player Constructor
+    public Player(string name, Position startPosition)
+    {
+        Name = name;
+        Position = startPosition;
+        GemCount = 0;
+    }
+    //direction reprensentation
+    public void move(char direction)
+    {
+        var position = Position;
+        var newX = position.X;
+        var newY = position.Y;
+        switch (direction)
+        {
+            case 'U': newX--; break;
+            case 'D': newX++; break;
+            case 'L': newY--; break;
+            case 'R': newY++; break;
+        }
+
+        if (newX > 5) newX = 5;
+        if (newY > 5) newY = 5;
+        if (newX < 0) newX = 0;
+        if (newY < 0) newY = 0;
+
+        Position = new Position(newX, newY);
+
+    }
+}
+
 class Program
 {
     //This method will start the application
